6ab0fbcee18d3f13163ab7775024ae2348da6302
AhmedKhaled590 <kahmd1444@gmail.com>
2022-10-06 13:00:17 +0200

    change order of getting IP Address

--- ipUtils.js
+++ ipUtils.js
@@ -1,21 +1,21 @@
 const getIp = (req) => {
   const ip =
-    req.headers["X-Client-IP"] ||
-    req.headers["x-forwarded-for"] ||
-    req.headers["CF-Connecting-IP"] ||
-    req.headers["Fastly-Client-IP"] ||
-    req.headers["True-Client-IP"] ||
-    req.headers["X-Real-IP"] ||
-    req.headers["X-Cluster-Client-IP"] ||
-    req.headers["X-Forwarded"] ||
-    req.headers["appengine-user-ip"] ||
+    req.ip ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress ||
     req.info.remoteAddress ||
+    req.headers["appengine-user-ip"] ||
+    req.headers["CF-Connecting-IP"] ||
     req.headers["CF-Pseudo-IPv4"] ||
     req.headers["request.raw"] ||
-    req.ip;
+    req.headers["X-Client-IP"] ||
+    req.headers["x-forwarded-for"] ||
+    req.headers["Fastly-Client-IP"] ||
+    req.headers["True-Client-IP"] ||
+    req.headers["X-Real-IP"] ||
+    req.headers["X-Cluster-Client-IP"] ||
+    req.headers["X-Forwarded"];
 
   return ip;
 };
